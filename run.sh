f95b85fa55d8cd0e198a5d131df9668e7e0ef280
Kiran Pachhai <kiran.pachhai@gmail.com>
2020-06-11 17:17:02 -0400

    Using same carrier data

--- run.sh
+++ run.sh
@@ -3,5 +3,6 @@
 docker container stop carrier-cast || true && docker container rm -f carrier-cast || true
 
 docker run -it --name carrier-cast \
+    -v "$PWD/.carrier-logs:/root/.carrier"     \
     -p 5000:5000 \
     tuumtech/carrier-cast:latest
